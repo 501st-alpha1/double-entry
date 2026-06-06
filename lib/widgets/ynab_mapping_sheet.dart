9631de6c0f44aacaf042e83618e94d8e9b95006c
Scott Weldon <open-source@scott-weldon.com>
2026-06-06 16:40:46 -0700

    Add missing import

--- lib/widgets/ynab_mapping_sheet.dart
+++ lib/widgets/ynab_mapping_sheet.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart' show Value;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../database/dao_providers.dart';
